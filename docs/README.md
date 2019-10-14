230b950130098d1fe67cfe2f30c28b6abc901ec1
Rodrigo Mendoza <eserod@gmail.com>
2019-10-14 01:11:35 -0500

    migrate

---
+++ docs/README.md
@@ -0,0 +1,1 @@
+Put pdfs in this folder
---
+++ research.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en"> 
+<head>
+	<title>Rodrigo Mendoza Smith</title>
+	<meta http-equiv="description" content="page dtdescription" />
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<link href="http://fonts.googleapis.com/css?family=Permanent+Marker" rel="stylesheet" type="text/css" />
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100|Pontano+Sans|Open+Sans:300|Raleway' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css" />
+  <?php include("includes/header.html");?>
+</head>
+<body>
+  <?php include("includes/research.html");?>
+</body>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  ga('create', 'UA-46133502-1', 'ox.ac.uk');
+  ga('send', 'pageview');
+</script>
+</html>
